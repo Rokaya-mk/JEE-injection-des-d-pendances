f921afe48052d617a51d39de97ab05a1bf6a6616
Rokaya-mk <mbairikrqia@gmail.com>
2026-04-18 00:21:33 +0100

    creer interfaces idao & imetier & les implementer

---
+++ src/main/java/metier/IMetierImpl.java
@@ -0,0 +1,18 @@
+package metier;
+
+import dao.IDao;
+
+public class IMetierImpl implements IMetier {
+    private IDao dao;
+
+    public IMetierImpl() {}
+    public IMetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+    @Override
+    public double calcule() {
+        return 0;
+    }
+
+
+}
